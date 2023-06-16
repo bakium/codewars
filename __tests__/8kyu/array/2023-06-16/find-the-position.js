6e8b26a9f5e75b44f466e3200d0e8fb18c7e6115
Bakium <124212108+bakium@users.noreply.github.com>
2023-06-16 10:38:28 +0200

    Add find the position simple solution

--- __tests__/8kyu/array/2023-06-16/find-the-position.js
+++ __tests__/8kyu/array/2023-06-16/find-the-position.js
@@ -5,13 +5,15 @@ Input :: "a"
 Ouput :: "Position of alphabet: */
 
 function position(letter) {
-    //Write your own Code!
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    return `Position of alphabet: ${alphabet.indexOf(letter.toLowerCase()) + 1}`
 }
+const { assert } = require('chai');
 
 describe("Tests", () => {
     it("test", () => {
-        Test.assertEquals(position("a"), "Position of alphabet: 1");
-        Test.assertEquals(position("z"), "Position of alphabet: 26");
-        Test.assertEquals(position("e"), "Position of alphabet: 5");
+        assert.strictEqual(position("a"), "Position of alphabet: 1");
+        assert.strictEqual(position("z"), "Position of alphabet: 26");
+        assert.strictEqual(position("e"), "Position of alphabet: 5");
     });
 });
