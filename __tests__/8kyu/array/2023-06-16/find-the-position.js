5aca678242225b50241c80029b924daf2c2241d9
Bakium <124212108+bakium@users.noreply.github.com>
2023-06-16 10:28:50 +0200

    Add find the position instructions

---
+++ __tests__/8kyu/array/2023-06-16/find-the-position.js
@@ -0,0 +1,17 @@
+/* When provided with a letter, return its position in the alphabet.
+
+Input :: "a"
+
+Ouput :: "Position of alphabet: */
+
+function position(letter) {
+    //Write your own Code!
+}
+
+describe("Tests", () => {
+    it("test", () => {
+        Test.assertEquals(position("a"), "Position of alphabet: 1");
+        Test.assertEquals(position("z"), "Position of alphabet: 26");
+        Test.assertEquals(position("e"), "Position of alphabet: 5");
+    });
+});
