d444adf66465dd48d50fcc162d52028b9c933abc
Bakium <124212108+bakium@users.noreply.github.com>
2023-07-12 22:58:42 +0200

    Add number to reverse array solution

--- __tests__/8kyu/array/2022-07-12/number-to-reverse-array.js
+++ __tests__/8kyu/array/2022-07-12/number-to-reverse-array.js
@@ -9,7 +9,7 @@ Example(Input => Output):
  */
 
 function digitize(n) {
-    //code here
+    return String(n).split('').reverse().map(n => Number(n))
 }
 
 const chai = require("chai");
@@ -21,14 +21,4 @@ describe("Basic tests", () => {
         assert.deepEqual(digitize(35231), [1, 3, 2, 5, 3]);
         assert.deepEqual(digitize(0), [0]);
     });
-}); const chai = require("chai");
-const assert = chai.assert;
-chai.config.truncateThreshold = 0;
-
-describe("Basic tests", () => {
-    it("Testing for fixed tests", () => {
-        assert.deepEqual(digitize(35231), [1, 3, 2, 5, 3]);
-        assert.deepEqual(digitize(0), [0]);
-    });
 });
-
