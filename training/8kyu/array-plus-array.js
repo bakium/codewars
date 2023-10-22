60c6ab1e6f521c79a512b980ab8a79a4040242b0
Bakium <124212108+bakium@users.noreply.github.com>
2023-10-22 15:50:51 +0200

    Add array plus array solution

--- training/8kyu/array-plus-array.js
+++ training/8kyu/array-plus-array.js
@@ -4,7 +4,7 @@ P.S. Each array includes only integer numbers. Output is a number too.
  */
 
 function arrayPlusArray(arr1, arr2) {
-    return arr1 + arr2; //something went wrong
+    return [...arr1, ...arr2].reduce((acc, current) => acc + current, 0)
 }
 
 const chai = require("chai");
