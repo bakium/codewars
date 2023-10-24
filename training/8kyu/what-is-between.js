76b138e6004dda8504defc4d970e467ad3294f05
Bakium <124212108+bakium@users.noreply.github.com>
2023-10-24 13:32:54 +0200

    Add what is between instructions

---
+++ training/8kyu/what-is-between.js
@@ -0,0 +1,22 @@
+/* Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
+
+For example:
+
+a = 1
+b = 4
+--> [1, 2, 3, 4]
+
+*/
+
+function between(a, b) {
+    // your code here
+}
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe("Basic tests", () => {
+    it("between(1, 4)", () => assert.deepEqual(between(1, 4), [1, 2, 3, 4]));
+    it("between(-2, 2)", () => assert.deepEqual(between(-2, 2), [-2, -1, 0, 1, 2]));
+});
