34099f37f5b821a4039f2e46dde95c4f41dbbb00
Bakium <124212108+bakium@users.noreply.github.com>
2023-10-30 11:43:52 +0100

    Add counting sheep instructions

---
+++ training/8kyu/counting-sheeps.js
@@ -0,0 +1,39 @@
+/* Description:
+
+Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+For example,
+
+[true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true]
+
+The correct answer would be 17.
+
+Hint: Don't forget to check for bad values like null/undefined
+ */
+
+function countSheeps(arrayOfSheep) {
+  // TODO May the force be with you
+}
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe("Tests", () => {
+  test("Fake test", () => { })
+  it("test", () => {
+    var array1 = [true, true, true, false,
+      true, true, true, true,
+      true, false, true, false,
+      true, false, false, true,
+      true, true, true, true,
+      false, false, true, true];
+
+    assert.deepEqual(countSheeps(array1), 17, "There are 17 sheeps in total")
+  });
+});
