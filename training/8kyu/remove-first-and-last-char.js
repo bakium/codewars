bbab3bb71eda72c0ee62b105225a30631d94ba0a
Bakium <124212108+bakium@users.noreply.github.com>
2023-11-05 18:41:34 +0100

    Add remove first and last char instructions

---
+++ training/8kyu/remove-first-and-last-char.js
@@ -0,0 +1,22 @@
+/* Description:
+
+It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string. You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+ */
+
+function removeChar(str) {
+    //You got this!
+};
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe("Tests", () => {
+    it("Fixed Tests", () => {
+        assert.strictEqual(removeChar('eloquent'), 'loquen');
+        assert.strictEqual(removeChar('country'), 'ountr');
+        assert.strictEqual(removeChar('person'), 'erso');
+        assert.strictEqual(removeChar('place'), 'lac');
+        assert.strictEqual(removeChar('ooopsss'), 'oopss');
+    });
+});
