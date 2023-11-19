7054fca54073d7c9b5d245d972c8ccc3141ddaba
Bakium <124212108+bakium@users.noreply.github.com>
2023-11-19 11:22:40 +0100

    Add number in expanded form instructions

---
+++ __tests__/6kyu/array/2023-10-19/number-in-expanded-form.js
@@ -0,0 +1,26 @@
+/* Write Number in Expanded Form
+
+You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+expandedForm(12); // Should return '10 + 2'
+expandedForm(42); // Should return '40 + 2'
+expandedForm(70304); // Should return '70000 + 300 + 4'
+
+NOTE: All numbers will be whole numbers greater than 0.
+
+If you liked this kata, check out part 2!!
+ */
+
+function expandedForm(num) {
+    // Your code here
+}
+
+const { assert } = require('chai');
+
+describe("Tests", () => {
+    it("test", () => {
+        assert.strictEqual(expandedForm(12), '10 + 2');
+        assert.strictEqual(expandedForm(42), '40 + 2');
+        assert.strictEqual(expandedForm(70304), '70000 + 300 + 4');
+    });
+});
