9406f4ad924131befbb3a14f79c8cb5dd996ccf9
Bakium <124212108+bakium@users.noreply.github.com>
2023-11-20 18:11:18 +0100

    Add tiny function solution

--- __tests__/6kyu/array/2023-10-20/tiny-function.js
+++ __tests__/6kyu/array/2023-10-20/tiny-function.js
@@ -10,7 +10,10 @@ Buffer and unicode escapes \ are banned to avoid cheesy solutions :)
  */
 
 
-f = _ => '' // tiny
+f=(x=1/0+"")=>x[6]+x[3]+x[4]+x[7]
+
+// too long !
+// String.fromCharCode(116,105,110,121)
 
 const chai = require("chai");
 const assert = chai.assert;
