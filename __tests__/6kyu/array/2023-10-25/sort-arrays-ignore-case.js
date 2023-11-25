d8482e567d82672ca08f22c77715f9f3d0bb7e34
Bakium <124212108+bakium@users.noreply.github.com>
2023-11-25 08:27:02 +0100

    Add sort array ignoring case instructions

---
+++ __tests__/6kyu/array/2023-10-25/sort-arrays-ignore-case.js
@@ -0,0 +1,23 @@
+/* Sort the given array of strings in alphabetical order, case insensitive. For example:
+
+["Hello", "there", "I'm", "fine"]  -->  ["fine", "Hello", "I'm", "there"]
+["C", "d", "a", "B"])              -->  ["a", "B", "C", "d"]
+
+ */
+
+// input: names - unsorted strings
+// output: case-agnostic sort
+sortme = function (names) {
+
+}
+
+const { assert } = require("chai")
+
+describe("Basic tests", () => {
+    it("Testing for fixed tests", () => {
+        assert.deepEqual(sortme(["Hello", "there", "I'm", "fine"]), ["fine", "Hello", "I'm", "there"])
+        assert.deepEqual(sortme(["C", "d", "a", "B"]), ["a", "B", "C", "d"])
+        assert.deepEqual(sortme(["CodeWars"]), ["CodeWars"])
+        assert.deepEqual(sortme([]), [])
+    })
+})
