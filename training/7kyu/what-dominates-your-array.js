81c765d07b61b484f1c3d7ff4cf5f53ddaa3dfd4
Bakium <124212108+bakium@users.noreply.github.com>
2023-12-14 09:19:06 +0100

    Add what dominate your array instructions

---
+++ training/7kyu/what-dominates-your-array.js
@@ -0,0 +1,24 @@
+/* Description:
+
+A zero-indexed array arr consisting of n integers is given. The dominator of array arr is the value that occurs in more than half of the elements of arr.
+For example, consider array arr such that arr = [3,4,3,2,3,1,3,3]
+The dominator of arr is 3 because it occurs in 5 out of 8 elements of arr and 5 is more than a half of 8.
+Write a function dominator(arr) that, given a zero-indexed array arr consisting of n integers, returns the dominator of arr. The function should return −1 if array does not have a dominator. All values in arr will be >=0.
+ */
+
+function dominator(arr) {
+    //code me
+}
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe("What dominates your array?", () => {
+    it("Fixed tests", () => {
+        assert.strictEqual(dominator([3, 4, 3, 2, 3, 1, 3, 3]), 3);
+        assert.strictEqual(dominator([1, 2, 3, 4, 5]), -1);
+        assert.strictEqual(dominator([1, 1, 1, 2, 2, 2]), -1);
+        assert.strictEqual(dominator([1, 1, 1, 2, 2, 2, 2]), 2);
+    })
+});
