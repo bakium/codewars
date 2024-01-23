fda4ab263b9f1aa028208a6072a760f3b6a1199d
Bakium <124212108+bakium@users.noreply.github.com>
2024-01-23 09:24:01 +0100

    Add sort by last char instructions

---
+++ training/7kyu/sort-by-last-char.js
@@ -0,0 +1,24 @@
+/* Description:
+
+Given a string of words (x), you need to return an array of the words, sorted alphabetically by the final character in each.
+
+If two words have the same last letter, they returned array should show them in the order they appeared in the given string.
+
+All inputs will be valid.
+*/
+
+function last(x) {
+
+}
+
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe("Sort by Last Char", () => {
+    it("Testing for fixed tests", () => {
+        assert.deepEqual(last('man i need a taxi up to ubud'), ['a', 'need', 'ubud', 'i', 'taxi', 'man', 'to', 'up']);
+        assert.deepEqual(last('what time are we climbing up the volcano'), ['time', 'are', 'we', 'the', 'climbing', 'volcano', 'up', 'what']);
+        assert.deepEqual(last('take me to semynak'), ['take', 'me', 'semynak', 'to']);
+    })
+});
